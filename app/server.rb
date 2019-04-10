66e4a19c4173fbc0453c81ba61c7d228b14aaf12
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2019-04-10 06:26:47 -0300

    Hello world with Sinatra

---
+++ app/server.rb
@@ -0,0 +1,13 @@
+require "sinatra"
+
+before do
+  if (! request.body.read.empty? and request.body.size > 0)
+    request.body.rewind
+    @params = Sinatra::IndifferentHash.new
+    @params.merge!(JSON.parse(request.body.read))
+  end
+end
+
+get "/" do
+  { hello: "world" }.to_json
+end
